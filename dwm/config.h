71841fe1b859bd3a114d57b7bb6c72f9e4f781e7
Michel Correa <michelosc@proton.me>
2023-01-10 12:35:52 -0300

    Adicionando modificações no dwm e no script de ram e updates

--- dwm/config.h
+++ dwm/config.h
@@ -30,7 +30,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Megasync",     NULL,       NULL,       0,            1,           -1 },
+	{ "MEGAsync",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
