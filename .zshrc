0a05335b76ea13c7dec26474e663c00e08bf1d2d
Michel Correa <michelosc@proton.me>
2023-01-13 09:59:05 -0300

    Modificações no dwm

---
+++ .zshrc
@@ -0,0 +1,39 @@
+# Created by newuser for 5.9
+
+eval "$(starship init zsh)"
+
+eval "$(zoxide init zsh)"
+
+export PATH=$HOME/.local/bin:$HOME/.cargo/bin:$PATH
+
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+source /home/michel/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+alias cat='bat'
+
+alias grep='rg'
+
+alias ls='exa --icons'
+
+alias ll='ls -al'
+
+alias du='dust'
+
+# alias find='fd'
+
+alias ps='procs'
+
+# alias sed='sd'
+
+alias top='ytop'
+
+
+
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- dwm/dwmblocks/blocks.def.h
+++ dwm/dwmblocks/blocks.def.h
@@ -1,10 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-  {" ", "~/.config/dwm/scripts/pacupdate", 3600, 0},
-  {" ", "~/.config/dwm/scripts/ram",	5,		0},
-  {" ", "~/.config/dwm/scripts/cpu", 5, 0},
-	{" ", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	/*Icon*/	/*Command*/		                  /*Update Interval*/	    /*Update Signal*/
+  {"🖥 ", "~/.config/dwm/scripts/cpu",        5,                      0 },
+  {"🧠 ", "~/.config/dwm/scripts/ram",	      5,		                  0 },
+  {" ", "~/.config/dwm/scripts/volume",      1,                      10},
+  {" ", "~/.config/dwm/scripts/pacupdate",   3600,                   0 },
+	{" ", "date '+%a %b %d |  %H:%M '",			  5,		                  0 },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
