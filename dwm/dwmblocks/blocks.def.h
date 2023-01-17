903c7d57be2e5766ca17f104e9aab7f1096a7344
Michel Correa <michelosc@proton.me>
2023-01-17 08:45:08 -0300

    Modificações no dwm

--- dwm/dwmblocks/blocks.def.h
+++ dwm/dwmblocks/blocks.def.h
@@ -2,7 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		                  /*Update Interval*/	    /*Update Signal*/
   {"^d^ ^b#cc241d^^c#1d2021^ 🖥 ^b#fb4934^ " , "~/.config/dwm/scripts/cpu",        5,                      0 },
-  {"^d^ ^b#689d6a^^c#1d2021^  ^b#8ec07c^ ", " ~/.config/dwm/scripts/ram",	      5,		                  0 },
+  {"^d^ ^b#689d6a^^c#1d2021^ 🧠 ^b#8ec07c^ ", " ~/.config/dwm/scripts/ram",	      5,		                  0 },
   {"^d^ ^b#b16286^^c#1d2021^  ^b#d3869b^ ", "~/.config/dwm/scripts/volume",     1,                      10},
   {"^d^ ^b#d65d0e^^c#1d2021^  ^b#fe8019^ ", "~/.config/dwm/scripts/pacupdate",  3600,                   0 },
 	{"^d^ ^b#d79921^^c#1d2021^  ^b#fabd2f^ ", "~/.config/dwm/scripts/date",			  3600,		                  0 },
