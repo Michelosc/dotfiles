a7fc7963deef099be8a77c9d593b7b3b52c32bd7
Michel Correa <michelosc@proton.me>
2023-01-22 14:57:56 -0300

    Adicionando configurações

--- dwm/dwmblocks/blocks.h
+++ dwm/dwmblocks/blocks.h
@@ -1,13 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		                  /*Update Interval*/	    /*Update Signal*/
-  {"^d^ ^b#cc241d^^c#1d2021^ 🖥 ^b#fb4934^ " , "~/.config/dwm/scripts/cpu",        5,                      0 },
-  {"^d^ ^b#689d6a^^c#1d2021^ 🧠 ^b#8ec07c^ ", " ~/.config/dwm/scripts/ram",	      5,		                  0 },
-  {"^d^ ^b#b16286^^c#1d2021^  ^b#d3869b^ ", "~/.config/dwm/scripts/volume",     1,                      10},
-  {"^d^ ^b#d65d0e^^c#1d2021^  ^b#fe8019^ ", "~/.config/dwm/scripts/pacupdate",  3600,                   0 },
-	{"^d^ ^b#d79921^^c#1d2021^  ^b#fabd2f^ ", "~/.config/dwm/scripts/date",			  3600,		                  0 },
-  {"^d^ ^b#458588^^c#1d2021^  ^b#83a598^ ", "~/.config/dwm/scripts/hour",       60,                     0 },
-  {"^d^ ", "",					0,	 0},
+  {"^c#4fffe4^  ", "~/.config/dwm/scripts/music", 5, 0},
+  {"^c#E06C75^ 🖥 ", "~/.config/dwm/scripts/cpu",        5,                      0 },
+  {"^c#689D6A^ 🧠 ", "~/.config/dwm/scripts/ram",	      5,		                  0 },
+  {"^c#C678DD^  ", "~/.config/dwm/scripts/volume",     1,                      10},
+  {"^c#D65D0E^  ", "~/.config/dwm/scripts/pacupdate",  3600,                   0 },
+	{"^c#fabd2f^  ", "~/.config/dwm/scripts/date",			  5,		                  0 },
+  {"^c#56B6C2^  ", "~/.config/dwm/scripts/hour",       60,                     0 }
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
