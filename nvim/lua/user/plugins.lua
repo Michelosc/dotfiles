6a267cca763fab0dca6ed976b8519ffff07a5a16
Michel Correa <micheloscbc@hotmail.com>
2023-11-14 11:06:19 -0300

    Adicionando plugin noice e modificando indentline

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -87,5 +87,21 @@ require("lazy").setup({
 	"jose-elias-alvarez/typescript.nvim",
 
 	-- Go
-	"fatih/vim-go"
+	"fatih/vim-go",
+
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    }
+  }
 })
